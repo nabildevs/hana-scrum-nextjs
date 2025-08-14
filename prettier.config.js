d3e84fcbb2c406209f074a85827a2fc46f2bba53
Hasbi Nabil <hasbinabil.work@gmail.com>
2025-08-14 15:13:13 +0700

    style: minor changes container style + configure prettier

--- prettier.config.js
+++ prettier.config.js
@@ -1,4 +1,8 @@
 /** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
 export default {
   plugins: ["prettier-plugin-tailwindcss"],
+  singleQuote: true,
+  useEditorConfig: false,
+  useTabs: true,
+  tabWidth: 4,
 };
